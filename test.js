8773c4d674cc3b566d35b7094232a20346af789d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:18 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import mergeAllRight from "./source"
+import mergeDeepLeft from "./source"
 
 test(({same, end}) => {
   same(
