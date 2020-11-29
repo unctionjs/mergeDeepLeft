9827bc4faf268fe64b4e610098721b6c5cc41d5b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:50 -0800

    Removing more bad types

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import mergeWith from "@unction/mergewith";
 import isObject from "@unction/isobject";
 import isArray from "@unction/isarray";
 
-export default function mergeDeepLeft<A> (left: Array<A> | Set<A> | RecordType<unknown, A> | string) {
-  return function mergeDeepLeftLeft (right: Array<A> | Set<A> | RecordType<unknown, A> | string): Array<A> | Set<A> | RecordType<unknown, A> | string {
+export default function mergeDeepLeft<A> (left: Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string) {
+  return function mergeDeepLeftLeft (right: Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string): Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string {
     if (isArray(left) && isArray(right)) {
       return mergeLeft(right)(left);
     }
