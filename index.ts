10bdb13d88b377920435939a25eca7cb4bc15b4e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-28 18:50:20 -0800

    Denormalizing some types to make things a little bit easier

--- index.ts
+++ index.ts
@@ -2,10 +2,9 @@ import mergeLeft from "@unction/mergeleft";
 import mergeWith from "@unction/mergewith";
 import isObject from "@unction/isobject";
 import isArray from "@unction/isarray";
-import {EnumerableType} from "./types";
 
-export default function mergeDeepLeft<A> (left: EnumerableType<A>) {
-  return function mergeDeepLeftLeft (right: EnumerableType<A>): EnumerableType<A> {
+export default function mergeDeepLeft<A> (left: Array<A> | Set<A> | RecordType<unknown, A> | string) {
+  return function mergeDeepLeftLeft (right: Array<A> | Set<A> | RecordType<unknown, A> | string): Array<A> | Set<A> | RecordType<unknown, A> | string {
     if (isArray(left) && isArray(right)) {
       return mergeLeft(right)(left);
     }
